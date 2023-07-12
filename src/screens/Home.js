3bf5d892a96ee14eec56d49baccc0f5ce677b0f3
Ngo Minh Vu <ngominhvu2003@gmail.com>
2023-07-12 07:56:06 +0700

    Create header

---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import '../styles/Home.css';
+// import DefaultTemplate from '../templates/DefaultTemplate';
+
+export default function Home() {
+    return (
+            <div id="banner">
+
+            </div>
+    )
+}
